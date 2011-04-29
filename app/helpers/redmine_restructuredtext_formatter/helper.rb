7d9555479dc8d33301fc94dbe72a0de91c544f3d
Mohammad Ebrahim Mohammadi Panah <ebrahim@mohammadi.ir>
2011-04-29 17:18:41 +0430

    Fixed toolbar images

--- app/helpers/redmine_restructuredtext_formatter/helper.rb
+++ app/helpers/redmine_restructuredtext_formatter/helper.rb
@@ -3,17 +3,14 @@ module RedmineRestructuredtextFormatter
     unloadable
 
     def wikitoolbar_for(field_id)
+      heads_for_wiki_formatter
       url = Redmine::Utils.relative_url_root +
         Engines::RailsExtensions::AssetHelpers.plugin_asset_path('redmine_restructuredtext_formatter', 'help', 'restructuredtext_syntax.html')
       help_link = l(:setting_text_formatting) + ': ' +
         link_to(l(:label_help), url,
         :onclick => "window.open(\"#{url}\", \"\", \"resizable=yes, location=no, width=300, height=640, menubar=no, status=no, scrollbars=yes\"); return false;")
 
-      javascript_include_tag('jstoolbar/jstoolbar') +
-        javascript_include_tag('restructuredtext', :plugin => 'redmine_restructuredtext_formatter') +
-        javascript_include_tag("jstoolbar/lang/jstoolbar-#{current_language}") +
-        javascript_include_tag("lang/restructuredtext-#{current_language}", :plugin => 'redmine_restructuredtext_formatter') +
-        javascript_tag("var toolbar = new jsToolBar($('#{field_id}')); toolbar.setHelpLink('#{help_link}'); toolbar.draw();")
+	  javascript_tag("var toolbar = new jsToolBar($('#{field_id}')); toolbar.setHelpLink('#{help_link}'); toolbar.draw();")
     end
 
 
@@ -22,8 +19,17 @@ module RedmineRestructuredtextFormatter
     end
 
     def heads_for_wiki_formatter
-      stylesheet_link_tag('jstoolbar') +
-        stylesheet_link_tag('restructuredtext', :plugin => 'redmine_restructuredtext_formatter')
-    end
+      unless @heads_for_wiki_formatter_included
+        content_for :header_tags do
+          javascript_include_tag('jstoolbar/jstoolbar') +
+          javascript_include_tag('restructuredtext', :plugin => 'redmine_restructuredtext_formatter') +
+          javascript_include_tag("jstoolbar/lang/jstoolbar-#{current_language.to_s.downcase}") +
+          javascript_include_tag("lang/restructuredtext-#{current_language.to_s.downcase}", :plugin => 'redmine_restructuredtext_formatter') +
+          stylesheet_link_tag('jstoolbar') +
+          stylesheet_link_tag('restructuredtext', :plugin => 'redmine_restructuredtext_formatter')
+        end
+		@heads_for_wiki_formatter_included = true
+      end
+	end
   end
 end
