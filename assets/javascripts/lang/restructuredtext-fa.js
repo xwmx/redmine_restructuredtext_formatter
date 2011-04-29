8e619ee199738f4504f79a5c1b59bcd6de0b961b
Mohammad Ebrahim Mohammadi Panah <ebrahim@mohammadi.ir>
2011-04-29 17:18:19 +0430

    Improved translation for pre button

--- assets/javascripts/lang/restructuredtext-fa.js
+++ assets/javascripts/lang/restructuredtext-fa.js
@@ -1,4 +1,4 @@
 // Translate untranslated strings of jstoolbar here
 jsToolBar.strings['Emphasized'] = 'تأکید';
-jsToolBar.strings['Preformatted'] = 'پردازش نشود';
-jsToolBar.strings['Un-preformatted'] = 'پردازش بشود';
+jsToolBar.strings['Preformatted'] = 'قالب‌دار';
+jsToolBar.strings['Un-preformatted'] = 'بی‌قالب';
