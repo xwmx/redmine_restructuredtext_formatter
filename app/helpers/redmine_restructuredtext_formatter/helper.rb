f4aa12e4cb29b2508484c0e387504f1ec2c99b55
Ebrahim Mohammadi <ebrahim@mohammadi.ir>
2011-05-14 01:50:42 +0430

    Initial implementation of a rich editor based on CodeMirror
    --HG--
    branch : codemirror

--- app/helpers/redmine_restructuredtext_formatter/helper.rb
+++ app/helpers/redmine_restructuredtext_formatter/helper.rb
@@ -10,7 +10,16 @@ module RedmineRestructuredtextFormatter
         link_to(l(:label_help), url,
         :onclick => "window.open(\"#{url}\", \"\", \"resizable=yes, location=no, width=300, height=640, menubar=no, status=no, scrollbars=yes\"); return false;")
 
-      javascript_tag("var toolbar = new jsToolBar($('#{field_id}')); toolbar.setHelpLink('#{help_link}'); toolbar.draw();")
+      javascript_tag(<<-EOD)
+      var textarea = $('#{field_id}');
+      var editor = CodeMirror.fromTextArea(textarea, {
+          onBlur: function() { editor.save(); },
+          lineNumbers: false,
+      });
+      var toolbar = new jsToolBar(editor, textarea);
+      toolbar.setHelpLink('#{help_link}');
+      toolbar.draw();
+      EOD
     end
 
 
@@ -22,13 +31,18 @@ module RedmineRestructuredtextFormatter
     def heads_for_wiki_formatter
       unless @heads_for_wiki_formatter_included
         content_for :header_tags do
-          javascript_include_tag('jstoolbar/jstoolbar') +
+          javascript_include_tag('jstoolbar', :plugin => 'redmine_restructuredtext_formatter') +
           javascript_include_tag('restructuredtext', :plugin => 'redmine_restructuredtext_formatter') +
           javascript_include_tag("jstoolbar/lang/jstoolbar-#{current_language.to_s.downcase}") +
           javascript_include_tag("lang/restructuredtext-#{current_language.to_s.downcase}", :plugin => 'redmine_restructuredtext_formatter') +
+          javascript_include_tag("codemirror-#{I18n.t(:direction)}", :plugin => 'redmine_restructuredtext_formatter') +
+          javascript_include_tag('codemirror-rst', :plugin => 'redmine_restructuredtext_formatter') +
           stylesheet_link_tag('jstoolbar') +
           stylesheet_link_tag('html4css1', :plugin => 'redmine_restructuredtext_formatter') +
-          stylesheet_link_tag('restructuredtext', :plugin => 'redmine_restructuredtext_formatter')
+          stylesheet_link_tag('restructuredtext', :plugin => 'redmine_restructuredtext_formatter') +
+          stylesheet_link_tag("codemirror-#{I18n.t(:direction)}", :plugin => 'redmine_restructuredtext_formatter') +
+          stylesheet_link_tag('codemirror-rst', :plugin => 'redmine_restructuredtext_formatter') +
+          '<style>.CodeMirror { border: 1px solid black; }</style>'
         end
         @heads_for_wiki_formatter_included = true
       end
