8bcc4501021f5aed053781edc236b1f4ac6192b7
Ebrahim Mohammadi <ebrahim@mohammadi.ir>
2011-05-14 19:36:16 +0430

    Support for easy migration from redmine_ckeditor (or any other HTML-backed formatter) implemented
    --HG--
    branch : codemirror

--- lib/redmine_restructuredtext_formatter/wiki_formatter.rb
+++ lib/redmine_restructuredtext_formatter/wiki_formatter.rb
@@ -3,6 +3,12 @@ require 'rbst'
 module RedmineRestructuredtextFormatter
   class WikiFormatter
     def initialize(text)
+      @content_is_html = text.strip[0..0] == '<'    # Check if format of content is HTML using a naive heuristic!
+      if @content_is_html
+        @text = text
+        return
+      end
+
       # TODO: Implement UI to config these and the CSS and store them in DB:
       @text = <<-EOD
       .. role:: underline
@@ -25,6 +31,7 @@ module RedmineRestructuredtextFormatter
     end
 
     def to_html(&block)
+      return @text if @content_is_html
       # TODO: Make the the parameters configurable:
       RbST.new(@text).to_html(:language => 'en', :part => 'fragment', :tab_width => 4)
     rescue => e
